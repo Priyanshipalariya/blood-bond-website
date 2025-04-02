4f65bb327132effed0513f4f8e8566c5dcd34d00
Priyanshi Palariya <priyapalariya26@gmail.com>
2025-04-02 10:55:50 +0530

    Created button component

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({className, children}) => {
+  return (
+        <button className={`font-mono text-white bg-red-600 hover:bg-red-700 border-transparent rounded-xl px-4 py-1  ${className}`}>{children}</button>
+  );
+};
+
+const ButtonLink = ({className, children}) =>{
+    return(
+        <button className={`font-mono text-red-600 text-lg hover:font-bold hover:text-xl hover:text-red-700 hover:underline ${className}`}>{children}</button>
+    )
+
+}
+
+export {Button, ButtonLink};

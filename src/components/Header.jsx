f05f11bad4228470634eabdcd1cd4d6c55ccac3e
Harshit Budhlakoti <harshitbudhlakoti00@gmail.com>
2025-04-02 22:47:30 +0530

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
 import { GiHamburgerMenu, GiHeartDrop } from "react-icons/gi";
 import { ImCross } from "react-icons/im";
-import { Button, ButtonLink } from "./ui/Button";
 import { Link, useLocation } from "react-router";
-import { FiUser } from "react-icons/fi";
-import { PiSignIn } from "react-icons/pi";
 
 const Navbar = () => {
   const navLinks = [
@@ -12,9 +9,7 @@ const Navbar = () => {
     { path: "/donate", label: "Donate" },
     { path: "/request", label: "Request" },
     { path: "/about", label: "About" },
-    { path: "/contact", label: "Contact" },
-    { path: "/signin", label: "Login" },
-    { path: "/signup", label: "Sign Up" },
+    { path: "/contact", label: "Contact" }
   ];
 
 
@@ -23,15 +18,15 @@ const Navbar = () => {
 
 
   return (
-    <header className="sticky top-0 z-50 w-full shadow-md px-5 lg:px-10">
-      <div className=" flex h-14 md:h-16 max-w-2xl mx-auto md:max-w-full items-center justify-between ">
+    <header className="fixed top-0 right-0 left-0 shadow-md px-5">
+      <div className=" flex h-14 md:h-16 items-center justify-between ">
         <div className="flex items-center gap-2 font-bold text-red-700">
           <GiHeartDrop className="text-3xl md:text-5xl" />
           <span className="text-2xl lg:text-3xl">Blood Bond</span>
         </div>
 
         <span
-          className={`block md:hidden transition-transform duration-300 hover:scale-110 text-red-700 text-2xl cursor-pointer }`}
+          className={`block md:hidden transition-transform duration-300 text-red-700 text-2xl cursor-pointer }`}
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
@@ -42,30 +37,13 @@ const Navbar = () => {
 
 
         {/* Desktop Menu*/}
-        <nav className="hidden md:flex items-center gap-4 lg:gap-6">
+        <nav className="hidden md:flex items-center md:text-md lg:text-lg gap-6 lg:gap-8">
           {navLinks.map(({ path, label }) => (
             <Link
               key={path}
               to={path}
               className={`hover:text-gray-400 duration-200 ${pathname === path ? "text-red-700 pointer-events-none" : ""}`}
-            >
-
-              {path === "/signup" ? (
-                <Button className=" lg:ml-2 flex gap-2 items-center">
-                  <FiUser className="hidden lg:block h-4 w-4" />
-                  {label}
-                </Button>
-              ) : path === "/signin" ? (
-                <ButtonLink className=" lg:ml-2 flex gap-2 items-center">
-                  <PiSignIn className="hidden lg:block h-4 w-4" />
-                  {label}
-                </ButtonLink>
-              ) : (
-                label
-              )}
-
-
-
+            >{label}
             </Link>
           ))}
         </nav>
@@ -75,7 +53,7 @@ const Navbar = () => {
 
       {/* SideBar of options */}
       <div
-        className={`fixed rounded-lg top-16 right-5 w-48 bg-white/70 shadow-lg md:hidden transition-all duration-300 ease-in-out transform ${isMenuOpen
+        className={`fixed rounded-lg top-16 right-5 w-48 bg-white/70 text-md shadow-lg md:hidden transition-all duration-300 ease-in-out transform ${isMenuOpen
           ? "opacity-100 scale-100 translate-y-0 pointer-events-auto"
           : "opacity-0 scale-95 -translate-y-4 pointer-events-none"
           }`}
@@ -91,19 +69,7 @@ const Navbar = () => {
 
               onClick={() => setIsMenuOpen(false)}
             >
-               {path === "/signup" ? (
-                <Button className=" lg:ml-2 flex gap-2 items-center w-full justify-center ">
-                  <FiUser className="h-4 w-4" />
-                  {label}
-                </Button>
-              ) : path === "/signin" ? (
-                <Button className=" lg:ml-2 flex gap-2 items-center w-full justify-center ">
-                  <PiSignIn className=" h-4 w-4" />
-                  {label}
-                </Button>
-              ) : (
-                label
-              )}
+              {label}
 
             </Link>
           ))}

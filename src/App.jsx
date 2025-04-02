1f2930f08dc75c76876bb103e38e4bdffc2df4cf
Priyanshi Palariya <priyapalariya26@gmail.com>
2025-04-02 23:42:20 +0530

    Added margin top to main page which contains routes mt-16

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="flex-grow ">
+      <div className="flex-grow mt-16">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/donate" element={<DonatePage />} />

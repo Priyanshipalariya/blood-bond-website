5d85f386651bec5ce2ac4321471c373cac779931
Priyanshi Palariya <priyapalariya26@gmail.com>
2025-04-04 02:17:07 +0530

    Added new button contrast component in button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
 const Button = ({className, children}) => {
   return (
-        <button className={`cursor-pointer font-mono text-white bg-red-600 hover:bg-red-700 border-transparent rounded-xl px-4 py-1  ${className}`}>{children}</button>
+        <button className={`cursor-pointer font-mono text-white bg-red-600 hover:bg-red-700 border-transparent rounded-md px-4 py-1  ${className}`}>{children}</button>
   );
 };
 
+const ButtonContrast = ({className, children}) => {
+  return (
+    <button className={`cursor-pointer font-mono text-red-600 bg-white  hover:text-black hover:bg-transparent border border-red-600 rounded-md px-4 py-1  ${className}`}>{children}</button>
+);
+}
 const ButtonLink = ({className, children}) =>{
     return(
         <button className={`cursor-pointer font-mono text-red-600 text-lg hover:font-bold hover:text-xl hover:text-red-700 hover:underline ${className}`}>{children}</button>
@@ -11,4 +16,4 @@ const ButtonLink = ({className, children}) =>{
 
 }
 
-export {Button, ButtonLink};
+export {Button, ButtonContrast, ButtonLink};

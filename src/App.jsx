332f007b3add25d3a3e622608ed35687813755aa
Harshit Budhlakoti <harshitbudhlakoti00@gmail.com>
2025-04-06 14:41:21 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import ContactPage from "./pages/ContactPage";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import FAQ from "./pages/FAQ";
 
 const App = () => {
   return (
@@ -25,7 +24,6 @@ const App = () => {
           <Route path="/information" element={<InformationPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
-          <Route path = "/faq" element = {<FAQ/>}/>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
